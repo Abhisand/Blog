b429eaa9ddab5548e3312c6e9dbcb9af74d7dc31
Abhishek <abhisand100@gmail.com>
2018-02-11 09:19:38 +0530

    jinja implementation

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response,get_object_or_404
 from django.http import  HttpResponse
 from . models import  Post
+
 
 posts = Post.objects.all()
 def index(request):
     return render(request, 'index.html',{'posts':posts})
 
-def post(request):
-    return HttpResponse("Hi")
+def post(request,slug):
+    print(slug)
+    return render_to_response('post.html',{
+        'post':get_object_or_404(Post,slug=slug)
+    })
+def about(request):
+    return render(request,'about.html')
 
--- ksit/urls.py
+++ ksit/urls.py
@@ -6,7 +6,8 @@ from blog import views as blog_views
 
 
 urlpatterns = [
-    url(r'^post/$',blog_views.post),
+    url(r'^post/(.*)$',blog_views.post),
+    url(r'^about/$',blog_views.about),
     url(r'^$',blog_views.index),
     url('admin/', admin.site.urls),
 ]
